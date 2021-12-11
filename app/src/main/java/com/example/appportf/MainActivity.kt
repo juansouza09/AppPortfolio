20fe2bbee96e7ba5ae957dfbf91b01b36c84d66c
juanpablo9 <juan.souza@dextra-sw.com>
2021-12-11 00:15:47 -0300

    splash funcionando

--- app/src/main/java/com/example/appportf/MainActivity.kt
+++ app/src/main/java/com/example/appportf/MainActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AppPortf)
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
